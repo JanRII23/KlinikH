37f4890bfd5ef9d44c6e988d6b4abb986c9cc923
JRII_Ataka <jrejasii@gmail.com>
2025-01-19 12:29:48 -0600

    V1 : initial db migration schema and setup

---
+++ KlinikH/KlinikH/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KlinikH.Models
+{
+    public class Category
+    {
+        //NOTE: this defines a primary key for the DB
+        [Key]
+        public int CategoryId { get; set; }
+
+        //NOTE: important to define the property type on the DB
+        [Column(TypeName = "nvarchar(50)")]
+        public required string Title {  get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public required string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(10)")]
+        public required string Type { get; set; } = "Expense";
+
+        //NOTE: assigning this to value in the DB column provides a default property
+    }
+}
