7784fa95f10c8c3bec55cc73539999e3d7aaaa33
JRII_Ataka <jrejasii@gmail.com>
2025-01-26 23:00:05 -0600

    V2 : adding file structure and app build for hot reloads support

--- KlinikH/KlinikH/Program.cs
+++ KlinikH/KlinikH/Program.cs
@@ -11,6 +11,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection"))
 );
 
+// Can actually leverage IIS Express or simply download the razor runtime package 
+
+//TODO: but this looks to break css mapping tho
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
