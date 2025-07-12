d546461d4effcbf5cec9458515184cc48701a338
JRII_Ataka <jrejasii@gmail.com>
2025-07-11 23:04:30 -0500

    V10 : defining application repositories

---
+++ HospitalApp/Hospital/Hospital.Models/Contacts.cs
@@ -0,0 +1,11 @@
+﻿namespace Hospital.Models
+{
+    public class Contacts
+    {
+        public int Id { get; set; }
+        public int HospitalId { get; set; }
+        public Hospital Hospital { get; set; }
+        public string Email { get; set; }
+        public string Phone {  get; set; }
+    }
+}
