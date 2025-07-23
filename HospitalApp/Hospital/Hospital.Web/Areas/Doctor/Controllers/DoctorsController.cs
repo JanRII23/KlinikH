db9dd164c95b8454207e8d5a583bce7fcdd762a5
JRII_Ataka <jrejasii@gmail.com>
2025-07-22 22:14:24 -0500

    V15 : adding some more views, controllers, and updated working DB schema

---
+++ HospitalApp/Hospital/Hospital.Web/Areas/Doctor/Controllers/DoctorsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Web.Areas.Doctor.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
