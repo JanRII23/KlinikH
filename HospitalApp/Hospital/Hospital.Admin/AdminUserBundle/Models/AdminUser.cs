afc18d13c295da7d6bfe009797f909da91010be4
JRII_Ataka <jrejasii@gmail.com>
2025-07-25 22:17:28 -0500

    V18: initial attempt at defining folder structure

---
+++ HospitalApp/Hospital/Hospital.Admin/AdminUserBundle/Models/AdminUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hospital.Admin.AdminUserBundle.Models
+{
+    public class AdminUser : IdentityUser
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public DateTime lastLogin { get; set; }
+        public DateTime createdOn { get; set; }
+
+        //TODO: think about other potential entities needed for the adminUser
+
+        //TODO: also think about migrating aftewards on this once hooking it up + squash commits
+    }
+}
