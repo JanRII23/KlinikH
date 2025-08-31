684ea9b605260f8a56d293f2ab09db65662e66f3
JRII_Ataka <jrejasii@gmail.com>
2025-08-31 17:13:36 -0500

    V21 : continue with react tutorial rank project

--- KlinikH/reactApp/ClientApp/src/components/RankItem.js
+++ KlinikH/reactApp/ClientApp/src/components/RankItem.js
@@ -1,4 +1,5 @@
 ﻿import React, { useState, useEffect } from 'react';
+import MovieImageArr from './MovieImages';
 
 const RankItems = () => {
 
@@ -17,9 +18,17 @@ const RankItems = () => {
 
     return (
         <main>
+            <div className = "items-not-ranked">
+
             {
-                (items.length > 0) ? items.map((item) => <h3>{item.title}</h3>):<div>Loading...</div>
+                    (items.length > 0) ? items.map((item) => 
+                        <div className = "unranked-cell">
+                            <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image}></img>
+                        </div>
+                    ) : <div>Loading...</div>
             }
+
+            </div>
         </main>
     )
 }
