484344da1cdb2dbdd1a27ca5bb370174872a8d58
JRII_Ataka <jrejasii@gmail.com>
2025-09-02 22:59:08 -0500

    V21 : completeting the SPA tutorial

--- KlinikH/reactApp/ClientApp/src/components/RankItem.js
+++ KlinikH/reactApp/ClientApp/src/components/RankItem.js
@@ -1,10 +1,34 @@
 ﻿import React, { useState, useEffect } from 'react';
 import MovieImageArr from './MovieImages';
+import RankingGrid from './RankingGrid';
 
 const RankItems = () => {
 
+    //TODO: what are these down here not really sure what the useEffect?
     const [items, setItems] = useState([]);
     const dataType = 1;
+
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+        const targetElm = ev.target;
+        if (targetElm.nodeName === "IMG") {
+            return false;
+        }
+        if (targetElm.childNodes.length === 0) {
+            var data = parseInt(ev.dataTransfer.getData("text").substring(5));
+            const transformedCollection = items.map((item) => (item.id === parseInt(data)) ?
+                { ...item, ranking: parseInt(targetElm.id.substring(5)) } : { ...item, ranking: item.ranking });
+            setItems(transformedCollection);
+        }
+    }
 
     useEffect(() => {
         fetch(`item/${dataType}`)
@@ -18,12 +42,13 @@ const RankItems = () => {
 
     return (
         <main>
+            <RankingGrid items={items} imgArr={MovieImageArr} drag={drag} allowDrop={allowDrop} drop={drop} />
             <div className = "items-not-ranked">
 
             {
                     (items.length > 0) ? items.map((item) => 
                         <div className = "unranked-cell">
-                            <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image}></img>
+                            <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image} style={{ cursor: "pointer" }} draggable="true" onDragStart={drag} />
                         </div>
                     ) : <div>Loading...</div>
             }
