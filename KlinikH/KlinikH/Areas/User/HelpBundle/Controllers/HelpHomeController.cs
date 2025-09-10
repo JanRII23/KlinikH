ad9543e09b1f7f288d1e722b6b67c5e9cdbfe19d
JRII_Ataka <jrejasii@gmail.com>
2025-09-09 23:06:50 -0500

    V22 : complete initial landing page layout

---
+++ KlinikH/KlinikH/Areas/User/HelpBundle/Controllers/HelpHomeController.cs
@@ -0,0 +1,17 @@
+﻿using KlinikH.Application.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KlinikH.Web.Areas.User.HelpBundle.Controllers
+{
+    [Area("User")]
+    public class HelpHomeController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View(CustomViewHelper.DefineCustomUserRoute("HelpBundle", "Index"));
+        }
+    }
+}
