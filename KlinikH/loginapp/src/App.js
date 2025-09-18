a179493ba68eeb8d12972429f46de3e540a1d5cf
JRII_Ataka <jrejasii@gmail.com>
2025-09-17 21:29:24 -0500

    V23 : adding new login SPA react page

--- KlinikH/loginapp/src/App.js
+++ KlinikH/loginapp/src/App.js
@@ -1,41 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
 
 function App() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        alert(`Logging in as ${username}`);
-        // TODO: call your ASP.NET MVC API
-    };
 
     return (
-        <div style={{ maxWidth: 400, margin: "50px auto" }}>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div style={{ marginBottom: 10 }}>
-                    <label>UsernameREACT</label>
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        className="form-control"
-                    />
-                </div>
-                <div style={{ marginBottom: 10 }}>
-                    <label>PasswordREACT</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="form-control"
-                    />
-                </div>
-                <button type="submit" className="btn btn-primary">
-                    Login
-                </button>
-            </form>
+        <div>
+            <LoginSignup/>
         </div>
     );
 }
